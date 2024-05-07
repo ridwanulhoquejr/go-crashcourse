02013525b9e56a3876b611889f270ce25ca8daba
ridwanulhoquejr <ridwanul.hoque.jr@gmail.com>
2024-05-07 20:41:41 +0600

    added empty interface example

--- main.go
+++ main.go
@@ -1,16 +1,26 @@
 package main
 
+import "fmt"
+
 func interfaceType(a Animal) {
 	a.Talk()
+	// based on the type of the instance, it will call the respective method
+	// wheather it is Human or Cat
+}
+
+// empty interface
+// if we want to pass any type to a function then it would be useful
+func emptyInterface(a interface{}) {
+	fmt.Println(a)
 }
 
 func main() {
 
 	// create a new instance of Human
-	h := Human{}
+	h := &Human{}
 
 	// create a new instance of Cat
-	c := Cat{}
+	c := &Cat{}
 
 	// call the interfaceType function with the Human instance
 	// and also with the Cat instance
@@ -18,9 +28,14 @@ func main() {
 	// since, both Human and Cat implements the Printer interface
 	// we can accept the type which implements the Animal interface
 
-	// so. first we pass the Human instance so it should print "Hello, I am a Human struct type"
-	// and then we pass the Cat instance so it should print "Hello, I am a Cat struct type"
-	interfaceType(&h)
-	interfaceType(&c)
+	// so. first we pass the Human instance so it should print "Hello, I am a Human struct type and i can talk"
+	// and then we pass the Cat instance so it should print "Hello, I am a Cat struct type but i can't talk"
+	interfaceType(h)
+	interfaceType(c)
+
+	// calling the emptyInterface function with a string, int and float64
+	emptyInterface("Hello, I am a string")
+	emptyInterface(10)
+	emptyInterface(10.5)
 
 }
